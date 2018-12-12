08f0e2aec0f243dd2b6bd78fce532f9dcb8aee87
x1234s <1908131339@qq.com>
2018-12-12 19:15:05 +0800

    add python init

---
+++ audio-streaming-client-python/process/conf.py
@@ -0,0 +1,18 @@
+conf = {
+    'basic_conf': {
+        'server': '172.18.53.17:31051',
+        'audio_file_path': '/Users/xiashuai01/TranFile/baidu/personal-code/'
+                           'shulingjie-grpc-java-client/audio-streaming-client/bj8k.wav'
+    },
+    'init_request_conf': {
+        'enable_chunk': True,
+        'enable_long_speech': True,
+        'enable_flush_data': True,
+        'product_id': '1903',
+        'sample_point_bytes': 2,
+        'send_per_seconds': 0.16,
+        'sleep_ratio': 1,
+        'app_name': 'python',
+        'log_level': 4
+    }
+}
---
+++ audio-streaming-client-python/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="grpc-python-client",
+    version="1.0.0",
+    description="for asr grpc python client",
+    author="Baidu gRPC",
+    url="www.baidu.com",
+    author_email="",
+    packages=find_packages(),
+    license="ABC",
+    python_requires=">=2.7",
+    install_requires=["grpcio>=1.1.3"],
+    entry_points={
+        'console_scripts': [
+            'protoc-gen-python_grpc=grpclib.plugin.main:main',
+        ],
+    },
+    classifiers=[
+        "Development Status :: %s" % "4 - beta",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"],
+)
