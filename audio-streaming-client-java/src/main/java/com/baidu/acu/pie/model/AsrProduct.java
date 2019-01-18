1500cca6d312a1ee0fae47e2391ffd4c64c4d399
cynricshu <cynricshu@gmail.com>
2019-01-18 15:29:17 +0800

    [java] add more product

--- audio-streaming-client-java/src/main/java/com/baidu/acu/pie/model/AsrProduct.java
+++ audio-streaming-client-java/src/main/java/com/baidu/acu/pie/model/AsrProduct.java
@@ -11,7 +11,10 @@ import lombok.Getter;
  */
 public enum AsrProduct {
     CUSTOMER_SERVICE("客服模型", "1903", 8000),
+    CUSTOMER_SERVICE_TOUR("客服模型：旅游领域", "1904", 8000),
+    CUSTOMER_SERVICE_STOCK("客服模型：股票领域", "1905", 8000),
     CUSTOMER_SERVICE_FINANCE("客服模型：金融领域", "1906", 8000),
+    CUSTOMER_SERVICE_ENERGY("客服模型：能源领域", "1907", 8000),
     INPUT_METHOD("输入法模型", "888", 16000),
     FAR_FIELD("远场模型", "1888", 16000);
 
