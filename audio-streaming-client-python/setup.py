9160c45b80cc1348727291e629159fe5ed060d97
xiashuai01 <xiashuai01@baidu.com>
2019-03-11 15:05:56 +0800

    add asr product map

--- audio-streaming-client-python/setup.py
+++ audio-streaming-client-python/setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
-# import os
+import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 changes = open(os.path.join(here, 'CHANGES.md')).read()
 
 setup(
     name="baidu-acu-asr",
-    version="1.1.2",
+    version="1.1.4",
     description="asr grpc client",
     long_description=changes,
+    long_description_content_type='text/markdown',
     author="Baidu",
     url="https://github.com/baidubce/pie/tree/master/audio-streaming-client-python-sdk",
     author_email="1908131339@qq.com",
