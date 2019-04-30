654b0678e483e75e4e52ea26dd4da92e7dadc72c
v_duping <553240007@qq.com>
2019-04-30 11:41:53 +0800

    add login and token

--- android-demo/app/src/main/java/com/pie/demo/Constants.java
+++ android-demo/app/src/main/java/com/pie/demo/Constants.java
@@ -5,7 +5,7 @@ package com.pie.demo;
  */
 public class Constants {
 
-    public static final String SERVER_IP_ADDR = "180.76.107.131";
+    public static final String SERVER_IP_ADDR = "127.0.0.1";
     public static final int SERVER_IP_PORT = 8050;
 
     public static final String ONEADDRESS = "oneaddress";
@@ -34,7 +34,7 @@ public class Constants {
 
 
     public static final String SERVER_IP_ADDR_PORT_TTS = "server_ip_addr_port_tts";
-    public static final String SERVER_IP_ADDR_PORT_TTS_DEF = "http://180.76.52.36:8088";
+    public static final String SERVER_IP_ADDR_PORT_TTS_DEF = "http://127.0.0.1:8088";
     public static final String SPD = "spd";
     public static final String PIT = "pit";
     public static final String VOl = "vol";
