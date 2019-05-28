cfa8bcc06e7536980ce6d878db1acbcdc0e98ad0
cynricshu <cynricshu@gmail.com>
2019-05-28 15:46:42 +0800

    [java-1.x.x] refactor: add more lombok annotations; change default value of keepAlive time out

--- audio-streaming-client-java/src/main/java/com/baidu/acu/pie/model/ChannelConfig.java
+++ audio-streaming-client-java/src/main/java/com/baidu/acu/pie/model/ChannelConfig.java
@@ -5,23 +5,26 @@ import java.util.concurrent.TimeUnit;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @Builder
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChannelConfig {
     @Builder.Default
     private final TimeSpan keepAliveTime = new TimeSpan(30, TimeUnit.SECONDS);
     @Builder.Default
-    private final TimeSpan keepAliveTimeout = new TimeSpan(5, TimeUnit.SECONDS);
+    private final TimeSpan keepAliveTimeout = new TimeSpan(20, TimeUnit.SECONDS);
 
-    @AllArgsConstructor
-    @Getter
+    @Data
     @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class TimeSpan {
-        private final long time;
-        private final TimeUnit timeUnit;
+        private long time;
+        private TimeUnit timeUnit;
     }
 }
