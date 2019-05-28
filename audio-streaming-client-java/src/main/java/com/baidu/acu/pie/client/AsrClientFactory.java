ad157b50ff2e9bf790dce8d0b3b267ca0c25146e
cynricshu <cynricshu@gmail.com>
2019-05-28 13:50:57 +0800

    [java-1.x.x] feat: add a new function to AsrClientFactory (close #72 btw) (#73)

--- audio-streaming-client-java/src/main/java/com/baidu/acu/pie/client/AsrClientFactory.java
+++ audio-streaming-client-java/src/main/java/com/baidu/acu/pie/client/AsrClientFactory.java
@@ -4,6 +4,7 @@ package com.baidu.acu.pie.client;
 
 import com.baidu.acu.pie.grpc.AsrClientGrpcImpl;
 import com.baidu.acu.pie.model.AsrConfig;
+import com.baidu.acu.pie.model.ChannelConfig;
 
 /**
  * AsrClientFactory
@@ -13,5 +14,9 @@ import com.baidu.acu.pie.model.AsrConfig;
 public class AsrClientFactory {
     public static AsrClient buildClient(AsrConfig asrConfig) {
         return new AsrClientGrpcImpl(asrConfig);
+    }
+
+    public static AsrClient buildClient(AsrConfig asrConfig, ChannelConfig channelConfig) {
+        return new AsrClientGrpcImpl(asrConfig, channelConfig);
     }
 }
