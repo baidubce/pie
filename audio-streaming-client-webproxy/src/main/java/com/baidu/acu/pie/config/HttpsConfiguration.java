85a1fc6c3c00f8996cb53e2843c2fadef6b77bc3
cynricshu <cynricshu@gmail.com>
2019-09-24 23:32:36 +0800

    [webproxy] refactor: make port configurable

--- audio-streaming-client-webproxy/src/main/java/com/baidu/acu/pie/config/HttpsConfiguration.java
+++ audio-streaming-client-webproxy/src/main/java/com/baidu/acu/pie/config/HttpsConfiguration.java
@@ -1,10 +1,10 @@
 package com.baidu.acu.pie.config;
 
-
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,25 +15,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HttpsConfiguration {
 
-    //  可以将http对外暴露的8080端口，转化为https的端口8081
+    @Value("${http.port}")
+    private int httpPort = 8080;
+
+    @Value("${server.port}")
+    private int serverPort = 8081;
+
     @Bean
-    public Connector connector(){
-        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    public Connector connector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(8080);
+        connector.setPort(httpPort);
         connector.setSecure(false);
-        connector.setRedirectPort(8081);
+        connector.setRedirectPort(serverPort);
         return connector;
     }
 
     @Bean
-    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
-        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector) {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
             protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint=new SecurityConstraint();
+                SecurityConstraint securityConstraint = new SecurityConstraint();
                 securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection=new SecurityCollection();
+                SecurityCollection collection = new SecurityCollection();
                 collection.addPattern("/*");
                 securityConstraint.addCollection(collection);
                 context.addConstraint(securityConstraint);
