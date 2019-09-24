02a1c490b86b6584e0932409473b8220115b4a7d
mazhenhua <mazhenhua@baidu.com>
2019-09-24 20:44:00 +0800

    1.  增加用户权限控制（后期待优化） 2.  将登录session与asr client管理分离 3.  增加微信操作相关接口 4.  asr支持二进制流和微信音频id两种方式解析

--- audio-streaming-client-webproxy/src/main/java/com/baidu/acu/pie/constant/Constant.java
+++ audio-streaming-client-webproxy/src/main/java/com/baidu/acu/pie/constant/Constant.java
@@ -4,8 +4,8 @@ package com.baidu.acu.pie.constant;
  * TODO 常量类 待优化
  */
 public class Constant {
-    public static String REQUEST_TYPE = "type";
-    public static String REQUEST_DATA = "data";
+    public static final String REQUEST_TYPE = "type";
+    public static final String REQUEST_DATA = "data";
 
-    public static String MUST_LOGIN_BEFORE_USE = "must login successfully before use asr";
+    public static final String MUST_LOGIN_BEFORE_USE = "must login successfully before use asr";
 }
