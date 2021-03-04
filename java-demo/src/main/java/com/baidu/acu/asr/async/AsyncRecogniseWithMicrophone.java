511fc9a63b83be79619cd70456b907eea79b0c34
xiashuai01 <xiashuai01@baidu.com>
2021-03-04 15:32:54 +0800

    [java] feat: add java microphone demo

---
+++ java-demo/src/main/java/com/baidu/acu/asr/async/AsyncRecogniseWithMicrophone.java
@@ -0,0 +1,147 @@
+package com.baidu.acu.asr.async;
+
+import com.baidu.acu.asr.model.Args;
+import com.baidu.acu.pie.client.AsrClient;
+import com.baidu.acu.pie.client.AsrClientFactory;
+import com.baidu.acu.pie.client.Consumer;
+import com.baidu.acu.pie.exception.AsrException;
+import com.baidu.acu.pie.model.AsrConfig;
+import com.baidu.acu.pie.model.RecognitionResult;
+import com.baidu.acu.pie.model.RequestMetaData;
+import com.baidu.acu.pie.model.StreamContext;
+import com.baidu.acu.pie.util.JacksonUtil;
+import org.joda.time.DateTime;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.TargetDataLine;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * AsyncRecogniseWithMicrophone
+ *
+ * @Author Xia Shuai(xiashuai01@baidu.com)
+ * @Create 2021/3/3 2:32 下午
+ */
+public class AsyncRecogniseWithMicrophone {
+    private static Args args;
+
+    private static String appName = "microphone";     // 根据自己需求命名
+
+    public static void main(String[] args) {
+        AsyncRecogniseWithMicrophone.args = Args.parse(args);
+        asyncRecognitionWithMicrophone();
+    }
+
+    private static AsrClient createAsrClient() {
+        // 创建调用asr服务的客户端
+        // asrConfig构造后就不可修改
+        return AsrClientFactory.buildClient(buildAsrConfig());
+    }
+
+    private static AsrConfig buildAsrConfig() {
+        // asrConfig构造后就不可修改
+        // 当使用ssl client时，需要配置字段sslUseFlag以及sslPath
+        return AsrConfig.builder()
+                .appName(appName)
+                .serverIp(args.getIp())
+                .serverPort(args.getPort())
+                .product(args.parseProduct(args.getProductId()))
+                .userName(args.getUsername())
+                .password(args.getPassword())
+                .build();
+    }
+
+    private static RequestMetaData createRequestMeta() {
+        RequestMetaData requestMetaData = new RequestMetaData();
+        requestMetaData.setSendPackageRatio(1);
+        requestMetaData.setSleepRatio(0);
+        requestMetaData.setTimeoutMinutes(120);
+        requestMetaData.setEnableFlushData(true);
+        // 随路信息根据需要设置
+        Map<String, Object> extra_info = new HashMap<>();
+        extra_info.put("demo", "java");
+        requestMetaData.setExtraInfo(JacksonUtil.objectToString(extra_info));
+
+        return requestMetaData;
+    }
+
+    /**
+     * 识别麦克风音频流
+     */
+    public static void asyncRecognitionWithMicrophone() {
+        AsrClient asrClient = createAsrClient();
+
+        RequestMetaData requestMetaData = createRequestMeta();
+
+        StreamContext streamContext = asrClient.asyncRecognize(new Consumer<RecognitionResult>() {
+            @Override
+            public void accept(RecognitionResult it) {
+                System.out.println(
+                        DateTime.now().toString() + "\t" + Thread.currentThread().getId() +
+                                " receive fragment: " + it);
+            }
+        }, requestMetaData);
+
+        streamContext.enableCallback(new Consumer<AsrException>() {
+            @Override
+            public void accept(AsrException e) {
+                if (e != null) {
+                    System.out.println(e);
+                }
+            }
+        });
+
+        AsrConfig asrConfig = buildAsrConfig();
+
+        TargetDataLine line = null;
+        AudioFormat audioFormat = new AudioFormat(
+                AudioFormat.Encoding.PCM_SIGNED,
+                asrConfig.getProduct().getSampleRate(),
+                16,
+                1,
+                2,    // (sampleSizeBits / 8) * channels
+                asrConfig.getProduct().getSampleRate(),
+                false);
+
+        DataLine.Info info = new DataLine.Info(
+                TargetDataLine.class,
+                audioFormat);
+
+        if (!AudioSystem.isLineSupported(info)) {
+            System.out.println("line not support");
+            return;
+        }
+
+        try {
+            line = (TargetDataLine) AudioSystem.getLine(info);
+
+            line.open(audioFormat, line.getBufferSize());
+            line.start();
+
+            int bufferLengthInBytes = asrClient.getFragmentSize();
+            byte[] data = new byte[bufferLengthInBytes];
+            System.out.println("start to record");
+            while ((line.read(data, 0, bufferLengthInBytes)) != -1L &&
+                    !streamContext.getFinishLatch().finished()) {
+                streamContext.send(data);
+            }
+
+            System.out.println(new DateTime().toString() + "\t" + Thread.currentThread().getId() + " send finish");
+            streamContext.complete();
+
+            // wait to ensure to receive the last response
+            streamContext.getFinishLatch().await();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            line.close();
+            asrClient.shutdown();
+        }
+        System.out.println("all task finished");
+    }
+
+}
---
+++ java-demo/src/main/java/com/baidu/acu/asr/model/Args.java
@@ -0,0 +1,47 @@
+package com.baidu.acu.asr.model;
+
+import com.baidu.acu.pie.model.AsrProduct;
+import lombok.Data;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+/**
+ * Args
+ *
+ * @Author Xia Shuai(xiashuai01@baidu.com)
+ * @Create 2021/3/3 2:41 下午
+ */
+@Data
+public class Args {
+    @Option(name = "-ip", required = true, usage = "asr server ip")
+    private String ip = "127.0.0.1";
+    @Option(name = "-port", required = true, usage = "asr server port")
+    private int port = 8051;
+    @Option(name = "-pid", required = true, usage = "asr product id")
+    private String productId = "1912";
+    @Option(name = "-username", required = true, usage = "username")
+    private String username = "";
+    @Option(name = "-password", required = true, usage = "password")
+    private String password = "";
+
+    public static Args parse(String[] args) {
+        Args iArgs = new Args();
+        CmdLineParser parser = new CmdLineParser(iArgs);
+        try {
+            parser.parseArgument(args);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(-1);
+        }
+        return iArgs;
+    }
+
+    public AsrProduct parseProduct(String productId) {
+        for (AsrProduct product : AsrProduct.values()) {
+            if (product.getCode().equals(productId)) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
